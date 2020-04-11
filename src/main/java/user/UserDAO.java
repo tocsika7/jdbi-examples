7d020fa375e742757f155b833e4b65b00f392973
Tóth Csaba <tothcsaba809@gmail.com>
2020-04-11 18:43:35 +0200

    Added the user package

---
+++ src/main/java/user/UserDAO.java
@@ -0,0 +1,48 @@
+package user;
+
+import org.jdbi.v3.sqlobject.config.RegisterBeanMapper;
+import org.jdbi.v3.sqlobject.customizer.Bind;
+import org.jdbi.v3.sqlobject.customizer.BindBean;
+import org.jdbi.v3.sqlobject.statement.GetGeneratedKeys;
+import org.jdbi.v3.sqlobject.statement.SqlQuery;
+import org.jdbi.v3.sqlobject.statement.SqlUpdate;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@RegisterBeanMapper(User.class)
+public interface UserDAO {
+
+    @SqlUpdate("""
+        CREATE TABLE user (
+            id IDENTITY PRIMARY KEY,
+            username VARCHAR NOT NULL,
+            password VARCHAR NOT NULL,
+            name VARCHAR NOT NULL,
+            email VARCHAR NOT NULL,
+            gender ENUM ('FEMALE','MALE'),
+            dob DATE NOT NULL,
+            enabled BOOLEAN NOT NULL
+        )
+        """
+    )
+    void createTable();
+
+    @SqlUpdate("INSERT INTO user (username, password, name, email, gender, dob, enabled)" +
+            " VALUES (:username, :password, :name, :email, :gender, :dob, :enabled )")
+    @GetGeneratedKeys
+    long insert(@BindBean User user);
+
+    @SqlQuery("SELECT * FROM user WHERE id = :id")
+    Optional<User> findById(@Bind("id") long id);
+
+    @SqlQuery("SELECT * FROM user WHERE username = :username")
+    Optional<User> findByUsername(@Bind("username") String username);
+
+    @SqlUpdate("DELETE FROM user WHERE username =:username")
+    void delete(@BindBean User user);
+
+    @SqlQuery("SELECT * FROM user")
+    List<User> list();
+}
